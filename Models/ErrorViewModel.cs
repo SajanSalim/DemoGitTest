cbd1cd123257148886b113ccdd9968741bf34fd8
pl1964 <sajansalimfaith@gmail.com>
2022-07-26 17:09:57 +0530

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace DemoGitTest.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
